7cdf573079ea8eb4057b6fb67f98820f22f082a1
Evie Kassela <evie@cslab.ece.ntua.gr>
2023-07-18 19:02:15 +0300

    Minor fix to make sure kafka server will always start

--- docker/kafka/start-kafka.sh
+++ docker/kafka/start-kafka.sh
@@ -147,6 +147,6 @@ if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
 fi
 
 export KAFKA_OPTS="-Djava.security.auth.login.config=/oauth/config/kafka_server_jaas.conf" # -Djavax.net.debug=ssl" #add-oauth
-
+sleep 60
 exec "$KAFKA_HOME/bin/kafka-server-start.sh" "$KAFKA_HOME/config/server.properties"
 
